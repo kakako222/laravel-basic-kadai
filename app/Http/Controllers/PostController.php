9dc8bf13afe570221bcc69e32a0273d3970569a8
kakako222 <mikanmikan428@gmail.com>
2024-11-17 12:56:58 +0900

    データの作成機能とバリデーションを実装しようの課題

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -20,20 +20,33 @@ class PostController extends Controller
         return view('posts.index', compact('posts'));
     }
 
-    
-    
     public function show($id) {
         // URL'/posts/{id}'の'{id}'部分と主キー（idカラム）の値が一致するデータをpostsテーブルから取得し、変数$postに代入する
         $post = Post::find($id);
-    
-        // データが存在しない場合、エラーメッセージを表示
-        if (!$post) {
-            return "Post not found.";  // データがない場合
-        }
     
         // 変数$postをposts/show.blade.phpファイルに渡す
         return view('posts.show', compact('post'));
     }
     
+    // 投稿作成フォームを表示
+    public function create()
+    {
+        return view('posts.create');  // posts/create.blade.php を表示
+    }
+
+    // 投稿データをデータベースに保存
+    public function store(Request $request) {
+        $validated = $request->validate([
+            'title' => 'required|max:20', //タイトル必須、最大２０文字
+            'content' => 'required|max:200', //内容必須、最大200文字
+        ]);
+
+        $post = Post::create([
+            'title' => $validated['title'],
+            'content' => $validated['content'],
+        ]);
+
+        return redirect('/posts');
+    }
 }
 
--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-        Route::get('/posts' , [PostController::class, 'index']);
-        Route::get('/posts/{id}', [PostController::class, 'show']);
-        Route::get('/posts/create', [PostController::class, 'create']);
-        Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
+
+Route::get('/posts/create', [PostController::class, 'create']);
+Route::get('/posts' , [PostController::class, 'index']);
+Route::get('/posts/{id}', [PostController::class, 'show']);
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
