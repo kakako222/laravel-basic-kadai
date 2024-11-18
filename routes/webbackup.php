80b90df1ad16871573bc7571c14c88f151fc4cf8
kakako222 <mikanmikan428@gmail.com>
2024-11-18 21:23:45 +0900

    Laravel Breezeで認証を実装しよう

--- routes/webbackup.php
+++ routes/webbackup.php
@@ -1,10 +1,9 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
  // ルーティングを設定するコントローラを宣言する
  use App\Http\Controllers\PostController;
-
+ 
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,18 +19,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-require __DIR__.'/auth.php';
 Route::get('/posts/create', [PostController::class, 'create']);
 Route::get('/posts' , [PostController::class, 'index']);
 Route::get('/posts/{id}', [PostController::class, 'show']);
-Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store')->middleware('POST');
+Route::post('/posts/store', [PostController::class, 'store'])->name('posts.store');
